1e4ff0967d3d07b07e5553e2592cf652a0827675
Sunil Kumar D M <monisha.chinta@gmail.com>
2017-07-14 20:33:51 -0700

    update db connections

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,11 +3,11 @@ const pgp = require('pg-promise')(options);
 
 // Database connection:
 const cn = {
-  host: 'localhost',
-  port: 1234,
-  user: 'postgres',
-  password: '152hsrdev',
-  database: 'timeit'
+  host: 'ec2-107-21-205-25.compute-1.amazonaws.com',
+  port: 5432,
+  user: 'redpdisvsoldfr',
+  password: '801fba483101970474743072df4f9b8f1f4ed1c83eaabb972bab503e7d641d4c',
+  database: 'd2k592bhu7a41p'
 };
 
 const db = pgp(cn);
